06bdf0d03d35ef9b60815486c833b581bc3edcfa
Coder-Iro <doohee006@gmail.com>
2020-05-06 15:28:07 +0900

    Initial Commit

---
+++ src/main/java/teamif/rpgstats/RPGStats.java
@@ -0,0 +1,17 @@
+package teamif.rpgstats;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RPGStats extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
