ca1f7165f60552a17e76ae367fb026808c7a596c
PatrickKR <mailpatrickkorea@gmail.com>
2020-05-07 15:02:14 +0900

    Added Role Interface, javadoc

--- src/main/java/io/teamif/rpgstats/plugin/RPGStatsPlugin.java
+++ src/main/java/io/teamif/rpgstats/plugin/RPGStatsPlugin.java
@@ -2,7 +2,14 @@ package io.teamif.rpgstats.plugin;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+/**
+ * Main class of the plugin
+ */
+@SuppressWarnings("unused")
 public final class RPGStatsPlugin extends JavaPlugin {
+    /**
+     * Called on enabling process
+     */
     @Override
     public void onEnable() {
         getLogger().info("========================================");
@@ -11,6 +18,9 @@ public final class RPGStatsPlugin extends JavaPlugin {
         getLogger().info("========================================");
     }
 
+    /**
+     * Called on disabling process
+     */
     @Override
     public void onDisable() {
         getLogger().info("========================================");
---
+++ src/main/java/io/teamif/rpgstats/roles/Role.java
@@ -0,0 +1,11 @@
+package io.teamif.rpgstats.roles;
+
+/**
+ * Role interface - will be used as RPG roles.
+ */
+public interface Role {
+    /**
+     * Name of the role
+     */
+    String name = "";
+}
