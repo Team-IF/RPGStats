0307eed29e1166494f51c58a8a3aca7b70644729
gplnature <gplnature@gmail.com>
2020-05-20 09:51:42 +0900

    Update IPlayer.java

--- src/main/java/io/teamif/rpgstats/player/IPlayer.java
+++ src/main/java/io/teamif/rpgstats/player/IPlayer.java
@@ -4,6 +4,8 @@ import io.teamif.rpgstats.enums.RoleType;
 import io.teamif.rpgstats.enums.TribeType;
 import io.teamif.rpgstats.roles.IRole;
 import io.teamif.rpgstats.roles.jobs.Rogue;
+import io.teamif.rpgstats.roles.jobs.Warrior;
+import io.teamif.rpgstats.roles.jobs.Wizard;
 import io.teamif.rpgstats.tribes.Demon;
 import io.teamif.rpgstats.tribes.Human;
 import io.teamif.rpgstats.tribes.ITribe;
@@ -26,13 +28,18 @@ public abstract class IPlayer {
      */
     public IPlayer(String tribeType, String roleName) {
         try {
-            final RoleType roleType = RoleType.valueOf(roleName);
-            final IRole role = new IRole(roleType) {
-                @Override
-                public RoleType getRoleType() {
-                    return super.getRoleType();
-                }
-            };
+            IRole role = null;
+            switch (RoleType.valueOf(roleName)) {
+                case Warrior:
+                    role = new Warrior();
+                    break;
+                case Wizard:
+                    role = new Wizard();
+                    break;
+                case Rogue:
+                    role = new Rogue();
+                    break;
+            }
 
             switch (TribeType.valueOf(tribeType)) {
                 case Jinn:
